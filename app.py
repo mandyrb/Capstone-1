ad85ac4ac210488abd6291701a7c22dafa251a0e
Mandy B <mandyblott@gmail.com>
2021-03-16 13:12:45 -0500

    Edit app.py to find database in the os, also added basic seed file to drop and add all tables for Heroku

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///paragraph_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', 'postgres:///paragraph_db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
